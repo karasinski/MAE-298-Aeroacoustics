955e8e8ed3fa5da214bed27f4f9ca7af0151f8cd
John Karasinski <karasinski@gmail.com>
2016-10-11 21:23:31 -0700

    create octave plot

--- Homework1/hw1.py
+++ Homework1/hw1.py
@@ -1,8 +1,12 @@
+import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy.signal
 import soundfile as sf
+
+sns.reset_orig()
+colors = sns.color_palette('deep', 10)
 
 ###############################################################################
 # Problem 1
@@ -25,7 +29,7 @@ minPeak = df.ix[df.Pascals.idxmin()]
 
 def plot1a():
     f, ax = plt.subplots()
-    df.plot(x='Time', y='Pascals', legend=False, ax=ax)
+    df.plot(x='Time', y='Pascals', legend=False, color=colors[0], ax=ax)
 
     plt.xlim(xmin=0.2)
     plt.ylim(-100, 100)
@@ -50,12 +54,12 @@ def plot1a():
              '{:2.2f} sec'.format(minPeak.Time - maxPeak.Time),
              horizontalalignment='center')
 
-    plt.vlines(x=maxPeak.Time, ymax=0, ymin=minPeak.Pascals, color='k', linestyles='--')
+    plt.vlines(x=maxPeak.Time, ymax=0, ymin=minPeak.Pascals, linestyles='--')
 
     plt.tight_layout()
     plt.savefig('tex/figs/Pascals_vs_Time.pdf')
-    plt.show()
-#plot1a()
+    plt.clf()
+plot1a()
 
 ################################################################################
 # Calculate the single sided power spectral density function
@@ -63,7 +67,7 @@ f, Gxx = scipy.signal.periodogram(df.Pascals, fs=fs, return_onesided=True)
 
 
 def plot1b():
-    plt.plot(f, Gxx, marker='o', color='k')
+    plt.plot(f, Gxx, marker='o', color=colors[0])
 
     plt.xlim(0, 50)
     plt.xlabel('Frequency (Hz)')
@@ -71,52 +75,82 @@ def plot1b():
 
     plt.tight_layout()
     plt.savefig('tex/figs/G_xx.pdf')
-    plt.show()
-#plot1b()
+    plt.clf()
+plot1b()
 
 ###############################################################################
 # Calculate and plot standard narrowband sound pressure level
 Pref = 20E-6
 
 T = len(df) / fs
-SPLf = 10 * np.log10((Gxx / T) / Pref ** 2)
-df = pd.DataFrame([f, SPLf]).T
+SPL = 10 * np.log10((Gxx / T) / Pref ** 2)
+df = pd.DataFrame([f, SPL]).T
 df.drop(0, inplace=True)
 df.columns = ['Frequency', 'SPL']
 
 
 def plot1c():
-    plt.semilogx(f, SPLf, 'k')
+    df.plot(x='Frequency', y='SPL', logx=True, color=colors[0])
 
-    plt.xlim(f[1], f[-1])
     plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Narrowband Sound Pressure Level')
+    plt.ylabel('Narrowband Sound Pressure Level (dB)')
 
     plt.tight_layout()
     plt.savefig('tex/figs/narrowband.pdf')
-    plt.show()
-#plot1c()
+    plt.clf()
+plot1c()
 
 ###############################################################################
 # Problem 2
 ###############################################################################
 
 
-def octaves(third=False, upper_frequency=10E3):
-    dx = 1.
-    if third:
-        dx = 1 / 3
-
+def generate_octaves(octave=1., upper_frequency=10E3):
     fc30 = 1000
     m = np.arange(1, 80)
 
-    center = fc30 * 2 ** (-10 + (m * dx))
-    upper = center * 2 ** (dx / 2)
-    lower = center / 2 ** (dx / 2)
+    center = fc30 * 2 ** (-10 + (m * octave))
+    upper = center * 2 ** (octave / 2)
+    lower = center / 2 ** (octave / 2)
 
     freqs = pd.DataFrame([lower, center, upper]).T
     freqs.columns = ['lower', 'center', 'upper']
 
     return freqs.query('upper < @upper_frequency')
 
-o = octaves(third=True)
+
+def sum_octaves(df, octave=1.):
+    octave = generate_octaves(octave, upper_frequency=df.Frequency.max())
+    res = []
+    for i, band in octave.iterrows():
+        b = df.query('@band.lower < Frequency < @band.upper')
+        Lp = 10 * np.log10((10 ** (b.SPL / 10)).sum())
+        res.append([band.center, Lp])
+
+    res = pd.DataFrame(res)
+    res.columns = ['Frequency', 'SPL']
+    return res
+
+third = sum_octaves(df, octave=1 / 3)
+full = sum_octaves(third, octave=1.)
+overall = 10 * np.log10((10 ** (full.SPL / 10)).sum())
+
+
+def plot2():
+    f, ax = plt.subplots()
+    df.plot(x='Frequency', y='SPL', logx=True,
+            color=colors[0], legend=False, ax=ax)
+    third.plot(x='Frequency', y='SPL', logx=True,
+               marker='o', color=colors[1], legend=False, ax=ax)
+    full.plot(x='Frequency', y='SPL', logx=True,
+              marker='o', color=colors[2], legend=False, ax=ax)
+
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Sound Pressure Level (dB)')
+    plt.legend(['Narrowband', 'One Third', 'Octave'])
+    plt.title('Overall SPL: {:2.2f} dB'.format(overall))
+
+    plt.tight_layout()
+    plt.savefig('tex/figs/octaves.pdf')
+    plt.clf()
+plot2()
